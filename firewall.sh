944b664328d28aa63506f60c462bba39582ffe8d
Luke Queenan <lukequeenan@gmail.com>
2012-01-27 12:20:10 -0800

    Added rules to drop incoming packets from port 0 along with packets to port 0

--- firewall.sh
+++ firewall.sh
@@ -17,14 +17,16 @@ iptables --append INPUT --protocol udp --dport 67:68 --sport 67:68 -j ACCEPT
 iptables --append INPUT --protocol tcp --dport 22 -j ACCEPT
 iptables --append OUTPUT --protocol tcp --sport 22 -j ACCEPT
 
-#Permit inbound and outbound http and https
+#Permit inbound and outbound http and https - DONE
 iptables --append INPUT --protocol tcp -m multiport --sports 80,443 -j ACCEPT
 iptables --append OUTPUT --protocol tcp -m multiport --dports 80,443 -j ACCEPT
 
-#Drop inbound traffic to port 80 from ports less than 1024
+#Drop inbound traffic to port 80 from ports less than 1024 - DONE
 iptables --append INPUT --protocol tcp --dport 80 --sport 0:1023 -j DROP
 
 #Drop all incoming packets from reserved port 0 as well as inbound traffic to
 #port 0
+iptables --append INPUT --sport 0 -j DROP
+iptables --append INPUT --dport 0 -j DROP
 
 #Keep track of www and ssh traffic versus the rest of the traffic on the system
