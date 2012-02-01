a436a35501c36e8bf6d143f1467982c6fe701b2d
Luke Queenan <lukequeenan@gmail.com>
2012-02-01 12:09:06 -0800

    Fixed a typo and changed the ordering of rules to provide stricter protection.

--- firewall.sh
+++ firewall.sh
@@ -12,11 +12,11 @@ iptables --new-chain sshOut
 iptables --new-chain wwwIn
 iptables --new-chain wwwOut
 
-#Send www and ssh traffic to custom chains - DONE
-iptables --append INPUT --protocol tcp --dport 22 -j sshIn
-iptables --append OUTPUT --prot0col tcp --sport 22 -j sshOut
-iptables --append INPUT --protocol tcp -m multiport --sports 80,443 -j wwwIn
-iptables --append OUTPUT --protocol tcp -m multiport --dports 80,443 -j wwwOut
+#Drop all incoming packets from port 0 and inbound traffic to port 0 - DONE
+iptables --append INPUT --protocol tcp -m multiport --ports 0 -j DROP
+
+#Drop inbound traffic to port 80 from ports less than 1024 - DONE
+iptables --append INPUT --protocol tcp --dport 80 --sport 0:1023 -j DROP
 
 #Allow DNS traffic on all adaptors - DONE
 iptables --append OUTPUT --protocol udp --dport 53 --sport 1024:65535 -j ACCEPT
@@ -24,6 +24,12 @@ iptables --append INPUT --protocol udp --sport 53 --dport 1024:65535 -j ACCEPT
 
 #Allow DHCP traffic on all adaptors - DONE
 iptables --append INPUT --protocol udp --dport 67:68 --sport 67:68 -j ACCEPT
+
+#Send www and ssh traffic to custom chains - DONE
+iptables --append INPUT --protocol tcp --dport 22 -j sshIn
+iptables --append OUTPUT --protocol tcp --sport 22 -j sshOut
+iptables --append INPUT --protocol tcp -m multiport --sports 80,443 -j wwwIn
+iptables --append OUTPUT --protocol tcp -m multiport --dports 80,443 -j wwwOut
 
 #Permit inbound and outbound ssh - DONE
 iptables --append sshIn --protocol tcp --dport 22 -j ACCEPT
@@ -33,8 +39,6 @@ iptables --append sshOut --protocol tcp --sport 22 -j ACCEPT
 iptables --append wwwIn --protocol tcp -m multiport --sports 80,443 -j ACCEPT
 iptables --append wwwOut --protocol tcp -m multiport --dports 80,443 -j ACCEPT
 
-#Drop inbound traffic to port 80 from ports less than 1024 - DONE
-iptables --append INPUT --protocol tcp --dport 80 --sport 0:1023 -j DROP
 
-#Drop all incoming packets from port 0 and inbound traffic to port 0 - DONE
-iptables --append INPUT --protocol tcp -m multiport --ports 0 -j DROP
+
+
