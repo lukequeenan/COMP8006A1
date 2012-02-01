cd2ce1d416b22fe948e5144c9d563434d2ae817d
Luke Queenan <lukequeenan@gmail.com>
2012-02-01 10:09:34 -0800

    Started on custom chains.

--- firewall.sh
+++ firewall.sh
@@ -6,6 +6,10 @@ iptables --policy INPUT DROP
 iptables --policy FORWARD DROP
 iptables --policy OUTPUT DROP
 
+#Create custom chains for keeping track of www and ssh traffic
+iptables --new-chain sshTraffic
+iptables --new-chain wwwTraffic 
+
 #Allow DNS traffic on all adaptors - DONE
 iptables --append OUTPUT --protocol udp --dport 53 --sport 1024:65535 -j ACCEPT
 iptables --append INPUT --protocol udp --sport 53 --dport 1024:65535 -j ACCEPT
@@ -24,9 +28,7 @@ iptables --append OUTPUT --protocol tcp -m multiport --dports 80,443 -j ACCEPT
 #Drop inbound traffic to port 80 from ports less than 1024 - DONE
 iptables --append INPUT --protocol tcp --dport 80 --sport 0:1023 -j DROP
 
-#Drop all incoming packets from reserved port 0 as well as inbound traffic to
-#port 0
-iptables --append INPUT --sport 0 -j DROP
-iptables --append INPUT --dport 0 -j DROP
+#Drop all incoming packets from port 0 as well as inbound traffic to port 0
+iptables --append INPUT --protocol tcp -m multiport --ports 0 -j DROP
 
 #Keep track of www and ssh traffic versus the rest of the traffic on the system
--- reset.sh
+++ reset.sh
@@ -1,8 +1,10 @@
 #Clear all filters
+echo "Flushing filters and deleting user chains"
 iptables --flush 
 iptables --delete-chain
 
 #Set everything to allow again
+echo "Setting default policy to ALLOW"
 iptables --policy INPUT ACCEPT
 iptables --policy FORWARD ACCEPT
 iptables --policy OUTPUT ACCEPT
